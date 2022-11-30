ae17acd3cd6b112a184adf98e35999f6f3a74e86
DhiazAngganaRamadhan <119508446+DhiazAngganaRamadhan@users.noreply.github.com>
2022-11-30 23:16:07 +0700

    Update tabel.py

--- tabel.py
+++ tabel.py
@@ -7,3 +7,13 @@ jml_data = int(input("Masukan Jumlah data : "))
 
 for i in range(jml_data):
     i+=1
+print("===========Data Mahasiswa ke -",i,"===========")
+    nm = str(input("Masukan Nama    : "))
+    np = (input("Masukan NPM     : "))
+    kls = (input("Masukan Kelas   : "))
+    jrsn = (input("Masukan Jurusan : "))
+    print("===========================================")
+    nama.append(nm)
+    npm.append(np)
+    kelas.append(kls)
+    jurusan.append(jrsn)
