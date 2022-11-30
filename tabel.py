38f53b5a3ab37e9a6331673011885e8c4e93fad8
Rendiirgi <119499982+Rendiirgi@users.noreply.github.com>
2022-11-30 23:08:22 +0700

    Create tabel.py

---
+++ tabel.py
@@ -0,0 +1,9 @@
+nama = []
+npm = []
+kelas = []
+jurusan = []
+print("===============TUGAS STRUKDAT===============")
+jml_data = int(input("Masukan Jumlah data : "))
+
+for i in range(jml_data):
+    i+=1
