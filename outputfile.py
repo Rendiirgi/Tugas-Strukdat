944bc7b7f6d69971037195041d74e8c934bffb7b
Kevin Haris Winarno <119269128+KevinHaris@users.noreply.github.com>
2022-12-02 22:22:33 +0700

    Update outputfile.py

--- outputfile.py
+++ outputfile.py
@@ -13,9 +13,12 @@ print("""
 | NAMA                  | NPM               |KELAS             |JURUSAN               |
 +-----------------------+-------------------+------------- ----+----------------------+""")
 
-for i in range (len(readnama)):
-    nama = str(readnama[i].strip())
+for i in range (len(readnama)): 
+    nama = str(readnama[i].strip()) 
     print('| '+nama,end='')
+    #Perulangan for yang mendeklarasika variabel i in range dengan menggunakan len untuk mengetahui panjang item pada variabel nama.
+    #Variabel nama akan sama dengan variabel readnama yang memiliki variabel i dan bertipe data string, dimana strip akan menghasilkan karakter besar atau kecil.
+    #Lalu akan mencetak variabel nama dengan yang akan membuat baris baru ketika dicetak.
     for j in range(20-1-len(nama)):
         print(' ',end ='')
     npm = str(readnpm[i].strip())
@@ -30,7 +33,10 @@ for i in range (len(readnama)):
     print('| '+jurusan,end='')
     for v in range(19-1-len(jurusan)):
          print(' ',end ='')
+    #pada line 22 sampai 35 perulangan for dengan variabel j, k, p, v dalam jarak 20-1-len dan 19-1-len dimasukkan variabel nya masing masing disesuaikan
+    #mencetak dengan print untuk baris barunya dan npm = string readnpm yang dimasukkan variabel i perulangannya dan strip sebagai menghasilkan karakternya besar atau kecil
+    #lalu cetak lagi dengan akhiran end=
     
-    print('|')
+    print('|') # berfungsi untuk penutup pada output ketika dicetak 
 
-print("+--------------------+----------------------------------------------------------------+")
+print("+--------------------+----------------------------------------------------------------+") 
