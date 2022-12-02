f587651911dce4d9b2bdaa1f1fc16eb02fbd7130
DhiazAngganaRamadhan <119508446+DhiazAngganaRamadhan@users.noreply.github.com>
2022-12-02 22:01:51 +0700

    Update outputfile.py

--- outputfile.py
+++ outputfile.py
@@ -2,3 +2,13 @@ fnama = open("nama.txt", "r")
 fnpm = open("npm.txt", "r")
 fkelas = open("kelas.txt", "r")
 fjurusan = open("jurusan.txt", "r")
+
+readnama = fnama.readlines()
+readnpm = fnpm.readlines()
+readkelas = fkelas.readlines()
+readjurusan = fjurusan.readlines()
+
+print("""
++-----------------------+------------------------------------ -+----------------------+
+| NAMA                  | NPM               |KELAS             |JURUSAN               |
++-----------------------+-------------------+------------- ----+----------------------+""")
