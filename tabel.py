d97ed9c00fc3a95e18633dd37bf8f986cfdf2722
DhiazAngganaRamadhan <119508446+DhiazAngganaRamadhan@users.noreply.github.com>
2022-12-02 23:01:08 +0700

    Update tabel.py

--- tabel.py
+++ tabel.py
@@ -9,10 +9,10 @@ for i in range(jml_data):
     i+=1
 """
     Untuk line 17 yang artinya kita membuat tampilan awal output dengan statement "Data Mahasiswa Ke-" serta ditambahkan nilai variable i dengan kondisi jika mengisi nilai variable i akan ditambah 1.
-    Tedapat line 18 yang artinya kita diperintahkan untuk menginputkan data berupa nama dengan inputan harus bertipedata string dan inputan tersebut akan disimpan pada variable nm.
+    Terdapat line 18 yang artinya kita diperintahkan untuk menginputkan data berupa nama dengan inputan harus bertipedata string dan inputan tersebut akan disimpan pada variable nm.
     Pada line 19 sampai dengan line 21 sama halnya seperti line 11 yaitu menginputkan data dimana masing-masing mempunyai penyimpanan inputan variable yang berbeda.
     Ada line 23 yang artinya variable nama akan menambahkan nilai array pada urutan akhir terhadap variable nm.
-    Di line 24 sampai dengan line 26 sama halnya seperti line 22 yaitu menambahkan nilai array pada urutan akhir pada variable yang berbeda-beda dan terhadap variable yang berbeda-beda.
+    Di line 24 sampai dengan line 26 sama halnya seperti line 22 yaitu menambahkan nilai array pada urutan akhir pada variable yang berbeda-beda dan juga terhadap variable yang berbeda-beda.
 """    
 print("===========Data Mahasiswa ke -",i,"===========")
     nm = str(input("Masukan Nama    : "))
