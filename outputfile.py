6de39f81b8e29dcdb89d5c63a0d125b0d3c65a0d
Kevin Haris Winarno <119269128+KevinHaris@users.noreply.github.com>
2022-12-02 22:05:43 +0700

    Update outputfile.py

--- outputfile.py
+++ outputfile.py
@@ -12,3 +12,25 @@ print("""
 +-----------------------+------------------------------------ -+----------------------+
 | NAMA                  | NPM               |KELAS             |JURUSAN               |
 +-----------------------+-------------------+------------- ----+----------------------+""")
+
+for i in range (len(readnama)):
+    nama = str(readnama[i].strip())
+    print('| '+nama,end='')
+    for j in range(20-1-len(nama)):
+        print(' ',end ='')
+    npm = str(readnpm[i].strip())
+    print('| '+npm,end='')
+    for k in range(19-1-len(npm)):
+         print(' ',end ='')
+    kelas = str(readkelas[i].strip())
+    print('| '+kelas,end='')
+    for p in range(19-1-len(kelas)):
+         print(' ',end ='')
+    jurusan = str(readjurusan[i].strip())
+    print('| '+jurusan,end='')
+    for v in range(19-1-len(jurusan)):
+         print(' ',end ='')
+    
+    print('|')
+
+print("+--------------------+----------------------------------------------------------------+")
